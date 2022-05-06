133bf2f7c7451b4fc9ee058fe5f9a028257efa08
Matt Bauer <matt@eightytwenty.dev>
2022-05-05 22:16:05 -0400

    Updated user api

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5001;
+const connectDB = require('./config/db');
+
+//Connect DB
+connectDB();
+
+//Init middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('API running');
 });
 
-app.listen(port, () => {
+//Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
